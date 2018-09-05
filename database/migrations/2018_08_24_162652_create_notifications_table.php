f2300bef79ba0d037754503dc7ff99f7010ce162
quanghv96 <quanghavan96@gmail.com>
2018-09-05 11:11:46 +0700

    create migration

---
+++ database/migrations/2018_08_24_162652_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('link');
+            $table->integer('status')->unsigned();
+            $table->integer('id_order')->unsigned();
+            $table->timestamp('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
